98c32f16b8b18bc019bfff2087e479215f263577
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-05-22 23:52:34 +0530

    complexity

---
+++ java_dsa/complexities/bigOexample.java
@@ -0,0 +1,16 @@
+package complexities;
+
+public class bigOexample {
+    //constant time = no matter the size of input the array asscesing any element takes a constant time
+
+    //eample
+    //accessing a specific elemnt in an array by index
+
+    public static void main(String[] args) {
+        int array[] = {
+            4,5,6,7,3
+    };
+
+        System.out.println(array[0]);
+}
+        }
