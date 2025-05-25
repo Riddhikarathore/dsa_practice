3431e17ed95d8ebec02ba0a4cc125ea30002d19e
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-05-25 23:25:06 +0530

    find largest element

--- java_dsa/arrays/largestelement.java
+++ java_dsa/arrays/largestelement.java
@@ -1,6 +1,20 @@
 package arrays;
 
 public class largestelement {
+public static int findlargest(int arr[]){
+    int large = arr[0];
+    for (int i = 0; i< arr.length;i++){
+        if(large<arr[i]){
+            large = arr[i];
+        }
+    }
+return (large);
+    }
+    public static void main(String[] args) {
+        int arr[] = {
+                4,5,8,9,1
+        };
+        System.out.println(findlargest(arr));
 
-
+    }
 }
