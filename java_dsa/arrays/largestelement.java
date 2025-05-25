a1dc78e5d115a2c7b9e09b655b028d10b6aed1cc
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-05-25 23:39:56 +0530

    findlargestelement

--- java_dsa/arrays/largestelement.java
+++ java_dsa/arrays/largestelement.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class largestelement {
 public static int findlargest(int arr[]){
     int large = arr[0];
@@ -8,13 +10,34 @@ public static int findlargest(int arr[]){
             large = arr[i];
         }
     }
+    System.out.println("the largest element in the array is : ");
 return (large);
     }
     public static void main(String[] args) {
-        int arr[] = {
-                4,5,8,9,1
-        };
+//        int arr[] ={
+//                1,4,3,6,8,7,3,9
+//        } ;
+      //  System.out.println(findlargest(arr));
+
+
+
+        // but if we want to take array from user then
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("enter the size of array ");
+        int size = sc.nextInt();
+
+        int arr[]= new int[size];
+
+        System.out.println("enter the elements ");
+        for (int i =0;i<size;i++){
+            arr[i]= sc.nextInt();
+        }
+
+
         System.out.println(findlargest(arr));
+
+
+
 
     }
 }
