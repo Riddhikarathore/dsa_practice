e1963b3fe7744aae5d87b0a2219a67ba3bfdac5a
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-05-26 23:12:01 +0530

    ReverseArray

--- java_dsa/arrays/reversearray.java
+++ java_dsa/arrays/reversearray.java
@@ -1,9 +1,40 @@
 package arrays;
 
+import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class reversearray {
+    public static void reverse(int arr[]) {
+       for(int i = 0;i< arr.length/2;i++){
+           int tem = arr[i];
+           arr[i]=arr[arr.length-i-1];
+           arr[arr.length-i-1]=tem;
+       }
+
+    }
+
+    public static void main(String[] args) {
 
 
+        // but if we want to take array from user then
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("enter the elements ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
 
 
+       reverse(arr);
 
+        for(int value : arr){
+            System.out.print(value +" \t ");
+        }
+
+    }
 }
