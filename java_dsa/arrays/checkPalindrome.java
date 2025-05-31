2650e198079f0f87230f0e1f11c91739a9ed8d8d
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-05-31 22:38:10 +0530

    check array is Palindrome

---
+++ java_dsa/arrays/checkPalindrome.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class checkPalindrome {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 1};
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            if (arr[i] != arr[arr.length - 1 - i]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The array is a Palindrome.");
+        } else {
+            System.out.println("The array is NOT a Palindrome.");
+        }
+    }
+}
