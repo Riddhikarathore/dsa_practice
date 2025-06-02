ecad34bdee161a3a48e57e88d9954a1cdffbf74e
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-06-02 22:49:35 +0530

    pattern 2

---
+++ java_dsa/patterns/pattern2.java
@@ -0,0 +1,19 @@
+package patterns;
+
+public class pattern2 {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        int space ;
+
+
+        for(i=1;i<=4;i++){
+            for (space =1 ;space<= 4-i;space++){
+                System.out.print(" ");
+            }
+        }
+        for(j=1;j<=i;j++){
+            System.out.print("*");
+        }
+    }
+}
