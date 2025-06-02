5e6e225992f1d1d515e90717f6c2b176cbf6ebd5
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-06-02 22:52:24 +0530

    pattern 3

---
+++ java_dsa/patterns/pattern3.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        for(i=1;i<=3;i++){
+            for(j=1;j<=4;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
