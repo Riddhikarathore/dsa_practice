3b3b33ef8bcc33bae8cd39987fdc1b5392b50eae
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-06-02 22:55:44 +0530

    incomplete

---
+++ java_dsa/patterns/pattern4.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class pattern4 {
+
+    public static void main(String[] args) {
+        int i;
+        int j;
+        int space;
+
+        for(i=1;i<=4;i++){
+
+        }
+    }
+}
