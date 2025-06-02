fce8707b867a09f947eea5741d61f3b7d78ed0e4
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-06-02 22:38:39 +0530

    star pattern 1

---
+++ java_dsa/patterns/pattern1.java
@@ -0,0 +1,16 @@
+package patterns;
+
+public class pattern1 {
+
+    public static void main(String[] args) {
+        int i ;
+        int j ;
+        for (i = 1;i<=4;i++){
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+}
