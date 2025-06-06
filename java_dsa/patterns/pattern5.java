c4fb92a29d81ec20ba9039150f944171e0d6a5a3
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-06-06 23:40:15 +0530

    number pattern1

---
+++ java_dsa/patterns/pattern5.java
@@ -0,0 +1,15 @@
+package patterns;
+
+public class pattern5 {
+    public static void main(String[] args) {
+        int i ;
+        int j ;
+
+        for(i=1;i<=4;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
