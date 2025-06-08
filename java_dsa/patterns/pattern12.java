11db7e314e76744cded0c05436cb1f4de2ced256
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-06-08 23:56:02 +0530

    star pattern

--- java_dsa/patterns/pattern12.java
+++ java_dsa/patterns/pattern12.java
@@ -4,14 +4,14 @@ public class pattern12 {
     public static void main(String[] args) {
         int n =5 ;
         for(int i = 1;i<=n;i++){
-            for(int j=1;j<=n-1;j++){
+            for(int j=1;j<=n-i;j++){
                 System.out.print(" ");
             }
             for(int j =1;j<=5;j++){
                 System.out.print("*");
             }
-
+            System.out.println();
         }
-        System.out.println();
+
     }
 }
