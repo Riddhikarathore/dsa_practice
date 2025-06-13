9e0d981ffb5f1adf62963ab0fe65abb7a6f57be6
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-06-13 23:36:22 +0530

    pattern23

---
+++ java_dsa/patterns/pattern23.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class pattern23 {
+
+    public static void main(String[] args) {
+        int i;
+        int j;
+        int space;
+
+        for(i=1;i<=4;i++){
+
+        }
+    }
+}
