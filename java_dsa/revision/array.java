a266ff287212c25146c2c5beed6969e61ee1558b
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-07-17 22:18:13 +0530

    remove pattern

--- java_dsa/revision/array.java
+++ java_dsa/revision/array.java
@@ -3,26 +3,42 @@ package revision;
 public class array {
     public static void main (String args[]){
         int arr[] = {3,2,1,5,2};
-        int largest = arr[0];
-        int secondlargest = -1;
-        for(int i =0;i< arr.length;i++){
-            if (largest< arr[i]){
-                largest = arr[i];
+      //  int largest = arr[0];
+        //int secondlargest = -1;
+        //for(int i =0;i< arr.length;i++){
+          //  if (largest< arr[i]){
+            //    largest = arr[i];
+            //}
+        //}
+
+        //for (int i = 0;i< arr.length;i++){
+          //  if(secondlargest < arr[i] && largest!=arr[i]){
+            //    secondlargest = arr[i];
+            //}
+        //}
+
+        //System.out.println("the largest element is : "+ largest);
+        //System.out.println("the second largest element is : "+ secondlargest);
+
+
+        for (int i = 0;i <10;i++){
+            if(arr[i]>=arr[i-1]){
+
+            }
+            else{
+
             }
         }
 
-        for (int i = 0;i< arr.length;i++){
-            if(secondlargest < arr[i] && largest!=arr[i]){
-                secondlargest = arr[i];
-            }
-        }
+        System.out.println("true");
 
-        System.out.println("the largest element is : "+ largest);
-        System.out.println("the second largest element is : "+ secondlargest);
+
 
     }
 }
 
 
-//check if the array is sorted
+
+
+
 
