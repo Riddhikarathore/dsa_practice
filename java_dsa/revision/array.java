265d63bc428384742be68a7098c12b478abb8367
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-07-22 22:45:57 +0530

    revision

--- java_dsa/revision/array.java
+++ java_dsa/revision/array.java
@@ -94,22 +94,131 @@ public class array {
     //left rotate the array by one place
 
 
-    public static void main(String[] args) {
-        int arr[] = {2,4,5,6,7,8};
-        int temp = arr[0];
+//    public static void main(String[] args) {
+//        int arr[] = {2,4,5,6,7,8};
+//        int temp = arr[0];
+//
+//        for(int i = 1; i< arr.length;i++){
+//            arr[i-1]=arr[i];
+//        }
+//        arr[arr.length-1]=temp;
+//
+//        System.out.println("after rotation ");
+//
+//        for(int i =0;i< arr.length;i++){
+//            System.out.println(arr[i]+"");
+//        }
+//
+//    }
 
-        for(int i = 1; i< arr.length;i++){
-            arr[i-1]=arr[i];
+// left rotate the elements to d place
+
+
+    //revision
+
+    //1 largest number
+
+//    public static void main(String args[]) {
+//        int arr[] = {1, 4, 2, 5, 89};
+//        int largest = arr[0];
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] > largest) {
+//                largest = arr[i];
+//            }
+//        }
+//        System.out.println("the largest element : ");
+//        System.out.println(largest);
+//
+//    }
+
+
+    //second largest
+
+//    public static void main (String args[]){
+//        int arr[]={1,4,6,8,9,43};
+//        int largest = arr[0];
+//        int secondlargest = -1;
+//
+//        for(int i = 0; i< arr.length;i++){
+//            if(arr[i]>largest){
+//                largest = arr[i];
+//
+//            }
+//        }
+//        System.out.println("the largest element :" + largest );
+//
+//        for(int i = 0;i< arr.length;i++){
+//            if(arr[i]>secondlargest && arr[i]!= largest){
+//                secondlargest = arr[i];
+//            }
+//        }
+//
+//        System.out.println("the second largest element is :"+ secondlargest);
+//
+//    }
+
+    //check array sorted
+
+//    public static void main (String args[]){
+//        int arr[]= {4,2,5,3,8};
+//        System.out.println(arr.length);
+//        boolean found = true;
+//        for(int i = 0;i<arr.length-1;i++){
+//            if(arr[i]>arr[i+1]){
+//                found = false;
+//            }
+//        }
+//        if(found){
+//            System.out.println("sorted");
+//        }
+//        else{
+//            System.out.println("unsorted");
+//        }
+//    }
+
+    //rotate array from one place
+
+//    public static void main (String args[]){
+//        int arr[]= {2,4,6,8,9,10};
+//
+//        int temp = arr[0];
+//        for(int i = 1;i< arr.length;i++){
+//            arr[i-1]=arr[i];
+//
+//        }
+//         arr[arr.length-1]=temp;
+//
+//        System.out.println("after rotate");
+//
+//        for(int k = 0;k< arr.length;k++){
+//            System.out.print(arr[k]+" ");
+//        }
+//
+//    }
+
+
+    //left rotate array by dth place
+
+    public static void main (String args[]){
+        int arr[]= {2,4,6,8,10};
+
+        int d = 9;
+
+        for (int i =0;i<9;i++){
+            int temp = arr[0];
+
+            for(int j= 0;j< arr.length-1;j++){
+                arr[j]=arr[j+1];
+            }
+
+            arr[arr.length-1]=temp;
         }
-        arr[arr.length-1]=temp;
 
-        System.out.println("after rotation ");
+        System.out.println("after rotation");
 
         for(int i =0;i< arr.length;i++){
-            System.out.println(arr[i]+"");
+            System.out.print(arr[i]);
         }
-
     }
-
-
 }
